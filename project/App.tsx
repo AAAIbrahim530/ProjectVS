b1a2a78b0d6e4cfcd7219cfcdd51f637868a4614
AAAIbrahim530 <07attiaibrahim@gmail.com>
2026-08-12 10:33:36 +0200

    added styling

--- project/App.tsx
+++ project/App.tsx
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button} from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.welcomeText}>Welcome your React App!</Text>
+
+      <Text style={styles.Headingtext}>Enter Name</Text>
+      <TextInput style={styles.InputBox} placeholder="Enter First Name" />
+      <Text style={styles.Headingtext}>Enter Surname</Text>
+      <TextInput style={styles.InputBox} placeholder="Enter Surname" />
+      <Button title = "Add User"/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  welcomeText: {
+    paddingTop: 40,
+    color:'purple',
+    fontWeight:'bold',
+    fontSize: 28,
+    textAlign: 'center',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  InputBox: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginBottom: 20,
+    width: '100%',
+  },
+  Headingtext: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
 });
