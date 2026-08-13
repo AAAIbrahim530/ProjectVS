756c375ce595b5afad1347e513afa0fd1e3fc39b
AAAIbrahim530 <07attiaibrahim@gmail.com>
2026-08-13 16:54:47 +0200

    added navigation between screens and fixed the errors

--- project/App.tsx
+++ project/App.tsx
@@ -3,21 +3,51 @@ import { useState } from 'react';
 import { StyleSheet, Text, TextInput, View, Button, Image} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+  Home: undefined;
+  ViewDetails: { NameSend: string; SurnameSend: string };
+};
+const Stack = createNativeStackNavigator<RootStackParamList>();
+type MainScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+type ViewDetailsProps = NativeStackScreenProps<RootStackParamList, 'ViewDetails'>;
+
+
 
 export default function App() {
-  const [Name,setName] = useState('');
-  const [Surname,setSurname] = useState('');
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MainScreen} />
+        <Stack.Screen name="ViewDetails" component={ViewDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+
+
+function MainScreen({ navigation }:
+  MainScreenProps) 
+{
+  const [Name, setName] = useState('');
+  const [Surname, setSurname] = useState('');
 
   console.log("App starting up");
   return (
     
-    <NavigationContainer>
+    
     <View>
+
       <View style ={styles.mainPicture}>
+
         <Image 
         style = {styles.imageSize}
         source={require('./assets/react-Native.png')}/>
+
       </View>
+
       <Text style={styles.welcomeText}>Welcome your React App!</Text>
 
 <View style= {styles.InputFlex}>
@@ -31,26 +61,39 @@ export default function App() {
 
 </View>
 
+<View style= {styles.InputFlex}>
+
       <Text style={styles.headingtext}>Enter Surname</Text>
       <TextInput style={styles.inputBox} placeholder="Enter Surname" 
       onChangeText = {newText => setSurname(newText.replace(/[^a-zA-Z\s]/g, ''))}
       autoCapitalize="words"
       autoComplete="name-family"
       keyboardType="default"/>
-     
-      <Button title = "Add User"
+
+   </View>
+
+  <Button title = "Add User"
       onPress={() => {
+        navigation.navigate('ViewDetails' , { NameSend : Name, SurnameSend : Surname });
         console.log("Name:" + Name + 
           "Surname:" + Surname);
-
-      }}/>
-
-      <StatusBar style="auto" />
+         }}/>
     </View>
-    </NavigationContainer>
   );
-}
+};
 
+function ViewDetails({ navigation ,route }: 
+  ViewDetailsProps) 
+  {
+  const NameGet = route.params.NameSend;
+  const SurnameGet = route.params.SurnameSend;
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Name : {NameGet} Surname: {SurnameGet}</Text>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   welcomeText: {
     paddingTop: 40,
@@ -85,4 +128,4 @@ const styles = StyleSheet.create({
     marginTop: 30,
     justifyContent: 'space-evenly',
   },
-});
+  });
